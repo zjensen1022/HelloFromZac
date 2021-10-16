7cd553a1367b79548c9dd57ed9aa054c0d32f716
BenjaminMayberry <92061983+BenjaminMayberry@users.noreply.github.com>
2021-10-16 14:32:57 -0600

    Update Main.java
    Updated greeting

--- src/jensen/Main.java
+++ src/jensen/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Zac.");
+        System.out.println("Hello Zac Jensen, My name is I AM BENNJAMIN MAYBERRY THE I.");
     }
 }
